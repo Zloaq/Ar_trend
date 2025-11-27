b844a3732a7889bb149e54ae04d38eef56aebfdc
Zloaq <tomotomoya0210@gmail.com>
2025-11-27 20:04:23 +0900

    commit_251127

--- cross_corr_all.py
+++ cross_corr_all.py
@@ -177,6 +177,7 @@ def do_scp_raw_fits(date_label: str, object_name: str, base_name_list: List[str]
     # シェルスクリプトを即席で作って、bash で実行する   
     script_content = f"""#!/bin/bash
 set -euo pipefail
+src="{RAID_PC}:{RAID_DIR}/{date_label}/spec/spec{date_label}-{{{num_min:04d}..{num_max:04d}}}.fits"
 dst="{dst_dir}"
 mkdir -p "$dst"
 echo "scp $src $dst"
